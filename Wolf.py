3675ac1db245870422d1df86e03a4b95af386f8d
Kimayaghcds08 <95491926+Kimayaghcds08@users.noreply.github.com>
2022-01-20 15:54:15 +0000

    Initial commit

---
+++ Wolf.py
@@ -0,0 +1,9 @@
+class wolf:
+   def __init__(self,_colorfur,_hight,_ eyecolor,_speed):
+     self.colorfur=_gray
+     self.hight=_2
+     self.eyecolor=_blue
+    self.speed=_maxspeed
+
+
+Class 
---
+++ main.py
@@ -0,0 +1,6 @@
+class wolf:
+   def __init__(self,_colorfur,_hight,_ eyecolor,_speed):
+     self.colorfur=gray
+     self.hight=2
+     self.eyecolor=blue
+    self.speed=max speed
